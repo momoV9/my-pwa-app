e0c99f1e6fcd42c59cde823951ce2025d3c37ccd
momov9 <r0905334@student.thomasmore.be>
2023-06-19 08:11:17 +0200

    added 2 buttons and some styling

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Welcome.module.css';
 import welcomeImage from './assets/welcome-image.jpg';
 
 const Welcome = () => {
+  const [showWelcomeText, setShowWelcomeText] = useState(true);
+
+  const toggleWelcomeText = () => {
+    setShowWelcomeText(!showWelcomeText);
+  };
+
   return (
     <div className={styles.welcomeContainer}>
       <img src={welcomeImage} alt="Welcome" className={styles.welcomeImage} />
       <h1 className={styles.welcomeHeading}>Welcome to Our PWA Application!</h1>
-      <p className={styles.welcomeText}>
-        Enjoy the functionality and convenience of our progressive web app.
-      </p>
+      {showWelcomeText && (
+        <p className={styles.welcomeText}>
+          Enjoy the functionality and convenience of our progressive web app.
+        </p>
+      )}
+      <div className={styles.buttonContainer}>
+        <button onClick={toggleWelcomeText}>
+          {showWelcomeText ? 'Close Welcome Text' : 'Open Welcome Text'}
+        </button>
+      </div>
     </div>
   );
 };
