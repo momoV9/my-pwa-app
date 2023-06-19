1597f7bb84d841b398f657b4c074026c5a568d25
momov9 <r0905334@student.thomasmore.be>
2023-06-19 08:05:18 +0200

    intitieel app

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,34 +1,27 @@
-// Service worker code
-
 const CACHE_NAME = 'my-pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/static/css/main.css',
-  '/static/js/main.js',
-  // Add more static assets to cache as needed
-];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+
+        './src/App.css',
+        './src/App.js',
+        './src/Welcome.js',
+        './src/assets/welcome-image.jpg',
+
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // If the requested resource is in the cache, return it
-        if (response) {
-          return response;
-        }
-
-        // If the requested resource is not in the cache, fetch it from the network
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
   );
 });
